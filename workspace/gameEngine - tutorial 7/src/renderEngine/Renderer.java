eeac70f59ffaac372e1966b895fe47ce31fc97e5
Edward Ryklin <ryklin@gmail.com>
2016-04-02 11:44:54 -0400

    Adding tutorial 7 - Introduced transformation matrixes and loading uniform variables into the vertex shader, but does not actually do anything to the renderer yet.

---
+++ workspace/gameEngine - tutorial 7/src/renderEngine/Renderer.java
@@ -0,0 +1,31 @@
+package renderEngine;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import models.RawModel;
+import models.TexturedModel;
+
+public class Renderer {
+	
+	public void prepare()
+	{
+		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+	}
+	
+	public void render(TexturedModel textureModel){
+		RawModel model = textureModel.getRawModel();
+		GL30.glBindVertexArray(model.getVaoID());
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureModel.getTexture().getID());
+		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+		GL30.glBindVertexArray(0);
+	}
+}
