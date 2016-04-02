f14749b87a87315d76aa61c48b69cadc5d26e25f
Edward Ryklin <ryklin@gmail.com>
2016-04-02 12:20:54 -0400

    Adding tutorial 8 (part 3) - Implemented the view matrix - Added the camera class with keyboard handlers for moving it

---
+++ workspace/gameEngine - tutorial 8c/src/engineTester/MainGameLoop.java
@@ -0,0 +1,72 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import entities.Entity;
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
+		
+		float[] vertices = { // vbo
+
+			//left bottom triangle
+			-0.5f, +0.5f, 0.0f, // v0 - x1, y1, z1
+			-0.5f, -0.5f, 0.0f, // v1 - x2, y2, z2
+			+0.5f, -0.5f, 0.0f, // v2 - x3, y3, z3
+			+0.5f, +0.5f, 0.0f, // v3 - x4, y4, z4
+		};
+		
+		
+		int[] indices = {
+				0,1,3, // top left triangle (v0, v1, v2)
+				3,1,2 // bottom right triangle (v3 v1 v2)
+		};
+		
+		float[] textureCoords = {
+				0,0,
+				0,1,
+				1,1,
+				1,0
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
+		Entity entity = new Entity(texturedModel, new Vector3f(0,0,-1),0,0,0,1);  // warning, do not set the z value to 0 or else the image texture will not be rendered inside the viewing plane because the projectionMatrix has been initialized.
+		Camera camera = new Camera();
+		
+		while(!Display.isCloseRequested()) {
+			entity.increasePosition(0, 0, -0.1f);
+			camera.move();
+			renderer.prepare();
+			//game logic
+			shader.start();
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
+			shader.stop();
+			DisplayManager.updateDisplay();
+		}
+
+		shader.cleanUp();
+		loader.cleanUp();
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
---
+++ workspace/gameEngine - tutorial 8c/src/shaders/StaticShader.java
@@ -0,0 +1,50 @@
+package shaders;
+
+import org.lwjgl.util.vector.Matrix4f;
+
+import entities.Camera;
+import toolbox.Maths;
+
+public class StaticShader extends ShaderProgram{
+
+	private static final String VERTEX_FILE= "src/shaders/vertexShader.txt"; 
+	private static final String FRAGMENT_FILE= "src/shaders/fragmentShader.txt"; 
+
+	private int location_transformationMatrix;
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
+	
+	public StaticShader(){
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+	
+	@Override
+	protected void bindAttributes()
+	{
+		super.bindAttribute(0, "position");
+		super.bindAttribute(1, "textureCoords");
+	}
+	
+	@Override
+	protected void getAllUniformLocations(){
+		
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+		location_viewMatrix = super.getUniformLocation("viewMatrix");
+		
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix){
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
+	
+	public void loadViewMatrix(Camera camera) {
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		super.loadMatrix(location_viewMatrix, viewMatrix);
+	}
+
+	public void loadProjectionMatrix(Matrix4f projection) {
+		super.loadMatrix(location_projectionMatrix, projection);
+	}
+
+}
