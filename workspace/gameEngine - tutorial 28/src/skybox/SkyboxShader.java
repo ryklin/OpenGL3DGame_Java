0523239eee4cff86ddcff025fae082045edb6a4e
Edward Ryklin <ryklin@gmail.com>
2016-04-26 18:29:54 -0400

    Adding tutorial 28 : Part 1 - Added fog color blending - Added rotation of the skybox by using the DisplayManager's clock

---
+++ workspace/gameEngine - tutorial 28/src/skybox/SkyboxShader.java
@@ -0,0 +1,62 @@
+package skybox;
+
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import renderEngine.DisplayManager;
+import shaders.ShaderProgram;
+import toolbox.Maths;
+
+public class SkyboxShader extends ShaderProgram{
+
+	private static final String VERTEX_FILE = "src/skybox/skyboxVertexShader.txt";
+	private static final String FRAGMENT_FILE = "src/skybox/skyboxFragmentShader.txt";
+	
+	private static final float ROTATE_SPEED = 1f; // 1 degree per second
+
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
+	private int location_fogColour;
+	
+	private float rotation = 0; // current rotation
+	
+	public SkyboxShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+	
+	public void loadProjectionMatrix(Matrix4f matrix){
+		super.loadMatrix(location_projectionMatrix, matrix);
+	}
+
+	public void loadViewMatrix(Camera camera){
+		Matrix4f matrix = Maths.createViewMatrix(camera);
+		// in a 4x4 matrix the last column determines the translation
+		matrix.m30 = 0;
+		matrix.m31 = 0;
+		matrix.m32 = 0;
+		
+		rotation += ROTATE_SPEED * DisplayManager.getFrameTimeSeconds(); // this is how you convert to degrees per second
+		Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0,1,0), matrix, matrix);
+		
+		super.loadMatrix(location_viewMatrix, matrix);
+	}
+	
+	public void loadFogColour(float r, float g, float b){
+		
+		super.loadVector(location_fogColour,  new Vector3f(r,g,b) ) ;
+	}
+	
+	@Override
+	protected void getAllUniformLocations() {
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+		location_viewMatrix = super.getUniformLocation("viewMatrix");
+		location_fogColour = super.getUniformLocation("fogColour");
+	}
+
+	@Override
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+	}
+
+}
