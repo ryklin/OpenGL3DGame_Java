071af2b0190585feedebc72246b95005ad25cd8a
Edward Ryklin <ryklin@gmail.com>
2016-05-02 20:14:13 -0400

    Added Water - tutorial 4 : projective texture - connected and then mixed the reflection and refraction textures in the shader
    Updated my camera's initial settings
    
    Activated my own camera pitch limits
    
    Moved my sun around a bit
    
    Removed the temporary GUI map textures for display the clipped fbo's
    
    Adding my own fps code calculation (might be wrong)

---
+++ workspace/Water - tutorial 4/src/renderEngine/Loader.java
@@ -0,0 +1,163 @@
+package renderEngine;
+
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL14;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+import de.matthiasmann.twl.utils.PNGDecoder;
+import de.matthiasmann.twl.utils.PNGDecoder.Format;
+import models.RawModel;
+import textures.TextureData;
+
+public class Loader {
+	
+	private List<Integer> vaos = new ArrayList<Integer>();
+	private List<Integer> vbos = new ArrayList<Integer>();
+	private List<Integer> textures = new ArrayList<Integer>();
+	
+	public RawModel loadToVAO(float[] positions, float[] textureCoords, float[] normals, int[] indices){
+		
+		int vaoID = createVAO();
+		bindIndicesBuffer(indices);
+		storeDataInAttributeList(0, 3, positions);
+		storeDataInAttributeList(1, 2, textureCoords);
+		storeDataInAttributeList(2, 3, normals);
+		unbindVAO();
+		return new RawModel(vaoID, indices.length);
+	}
+	
+	public RawModel loadToVAO(float[] positions, int dimensions){
+		int vaoID = createVAO();
+		storeDataInAttributeList(0, dimensions, positions);
+		unbindVAO();
+		return new RawModel(vaoID, positions.length/dimensions);
+	}
+	
+	public int loadTexture(String fileName){
+		Texture texture = null; // this Texture object is provided by slick utils and is not cross platform compatible
+		try{
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + fileName + ".png"));
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Tried to load texture " + fileName + ".png , didn't work");
+			System.exit(-1);
+		}
+		textures.add(texture.getTextureID());
+		return texture.getTextureID();
+	}
+	
+	public void cleanUp(){
+		for(int vao:vaos){
+			GL30.glDeleteVertexArrays(vao);
+		}
+		
+		for(int vbo:vbos){
+			GL15.glDeleteBuffers(vbo);
+		}
+		for(int texture:textures){
+			GL11.glDeleteTextures(texture);
+		}
+		
+	}
+	
+	public int loadCubeMap(String[] textureFiles)
+	{
+		int texID = GL11.glGenTextures();
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texID);
+		
+		for (int i = 0; i<textureFiles.length;i++){
+			TextureData data = decodeTextureFile("res/" + textureFiles[i] + ".png");
+			// notice the + i, as explained in the video.
+			GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, 0, GL11.GL_RGBA, data.getWidth(), data.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.getBuffer());
+		}
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+		textures.add(texID);
+		return texID;
+	}
+	
+	private TextureData decodeTextureFile(String fileName) {
+		int width = 0;
+		int height = 0;
+		ByteBuffer buffer = null;
+		try {
+			FileInputStream in = new FileInputStream(fileName);
+			PNGDecoder decoder = new PNGDecoder(in);
+			width = decoder.getWidth();
+			height = decoder.getHeight();
+			buffer = ByteBuffer.allocateDirect(4 * width * height);
+			decoder.decode(buffer, width * 4, Format.RGBA);
+			buffer.flip();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Tried to load texture " + fileName + ", didn't work");
+			System.exit(-1);
+		}
+		return new TextureData(buffer, width, height);
+	}
+	
+	private int createVAO() {
+		int vaoID = GL30.glGenVertexArrays();
+		vaos.add(vaoID);
+		GL30.glBindVertexArray(vaoID);
+		return vaoID;		
+	}
+	
+	private void storeDataInAttributeList(int attributeNumber, int coordinateSize, float[] data){
+		int vboID = GL15.glGenBuffers();
+		vbos.add(vboID);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+		FloatBuffer buffer = storeDataInFloatBuffer(data);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(attributeNumber, coordinateSize, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+	}
+	
+	private void unbindVAO(){
+		GL30.glBindVertexArray(0);
+	}
+	
+	private void bindIndicesBuffer(int[] indices){
+		int vboID = GL15.glGenBuffers();
+		vbos.add(vboID);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER,	vboID);
+		IntBuffer buffer = storeDataInIntBuffer(indices);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+	}
+	
+	private IntBuffer storeDataInIntBuffer(int[] data){
+		IntBuffer buffer = BufferUtils.createIntBuffer( data.length	);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
+	}
+	private FloatBuffer storeDataInFloatBuffer(float[] data){
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
+	}
+
+}
---
+++ workspace/Water - tutorial 4/src/water/WaterShader.java
@@ -0,0 +1,55 @@
+package water;
+
+import org.lwjgl.util.vector.Matrix4f;
+import shaders.ShaderProgram;
+import toolbox.Maths;
+import entities.Camera;
+
+public class WaterShader extends ShaderProgram {
+
+	private final static String VERTEX_FILE = "src/water/waterVertex.txt";
+	private final static String FRAGMENT_FILE = "src/water/waterFragment.txt";
+
+	private int location_modelMatrix;
+	private int location_viewMatrix;
+	private int location_projectionMatrix;
+	private int location_reflectionTexture;
+	private int location_refractionTexture;
+
+	public WaterShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+
+	@Override
+	protected void bindAttributes() {
+		bindAttribute(0, "position");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_projectionMatrix = getUniformLocation("projectionMatrix");
+		location_viewMatrix = getUniformLocation("viewMatrix");
+		location_modelMatrix = getUniformLocation("modelMatrix");
+		location_reflectionTexture = getUniformLocation("reflectionTexture");
+		location_refractionTexture = getUniformLocation("refractionTexture");
+	}
+
+	public void connectTextureUnits() {
+		super.loadInt(location_reflectionTexture, 0);
+		super.loadInt(location_refractionTexture, 1);	
+	}
+	
+	public void loadProjectionMatrix(Matrix4f projection) {
+		loadMatrix(location_projectionMatrix, projection);
+	}
+	
+	public void loadViewMatrix(Camera camera){
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		loadMatrix(location_viewMatrix, viewMatrix);
+	}
+
+	public void loadModelMatrix(Matrix4f modelMatrix){
+		loadMatrix(location_modelMatrix, modelMatrix);
+	}
+
+}
