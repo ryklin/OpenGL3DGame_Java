ebc4e1e8e329e3ebff3c8780c489e91d5095afe4
Edward Ryklin <ryklin@gmail.com>
2016-05-02 21:40:03 -0400

    Adding Water - tutorial 5 DUDV maps with move factor - My trees don't reflect exactly the same way as in the video, they seem to be "sparser". - Also, my height map doesn't seem to be exactly the same as in the tutorial. I commented on this in forum and I might get a response on how to fix it.

---
+++ workspace/Water - tutorial 5/src/water/WaterShader.java
@@ -0,0 +1,64 @@
+package water;
+
+import org.lwjgl.util.vector.Matrix4f;
+import shaders.ShaderProgram;
+import toolbox.Maths;
+import entities.Camera;
+
+public class WaterShader extends ShaderProgram {
+
+	private final static String VERTEX_FILE = "src/water/waterVertex.txt";
+	private final static String FRAGMENT_FILE = "src/water/waterFragment.txt";
+
+	private int location_modelMatrix;
+	private int location_viewMatrix;
+	private int location_projectionMatrix;
+	private int location_reflectionTexture;
+	private int location_refractionTexture;
+	private int location_dudvMap;
+	private int location_moveFactor;
+
+	public WaterShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+
+	@Override
+	protected void bindAttributes() {
+		bindAttribute(0, "position");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_projectionMatrix = getUniformLocation("projectionMatrix");
+		location_viewMatrix = getUniformLocation("viewMatrix");
+		location_modelMatrix = getUniformLocation("modelMatrix");
+		location_reflectionTexture = getUniformLocation("reflectionTexture");
+		location_refractionTexture = getUniformLocation("refractionTexture");
+		location_dudvMap = getUniformLocation("dudvMap");
+		location_moveFactor = getUniformLocation("moveFactor");
+	}
+
+	public void connectTextureUnits() {
+		super.loadInt(location_reflectionTexture, 0);
+		super.loadInt(location_refractionTexture, 1);
+		super.loadInt(location_dudvMap, 2);
+	}
+	
+	public void loadMoveFactor(float factor){
+		super.loadFloat(location_moveFactor, factor);
+	}
+
+	public void loadProjectionMatrix(Matrix4f projection) {
+		loadMatrix(location_projectionMatrix, projection);
+	}
+	
+	public void loadViewMatrix(Camera camera){
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		loadMatrix(location_viewMatrix, viewMatrix);
+	}
+
+	public void loadModelMatrix(Matrix4f modelMatrix){
+		loadMatrix(location_modelMatrix, modelMatrix);
+	}
+
+}
